5a0129f52b1872f3715f2a0bcbe38462f19057c9
hlebnoe_pole <glebpavlyuk@gmail.com>
2024-05-30 23:34:08 +0300

    resize desktop window

--- desktop/src/ru/samsung/gamestudio/DesktopLauncher.java
+++ desktop/src/ru/samsung/gamestudio/DesktopLauncher.java
@@ -2,14 +2,13 @@ package ru.samsung.gamestudio;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import ru.samsung.gamestudio.MyGdxGame;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
         config.setTitle("Space cleaner");
-        config.setWindowedMode(GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT);
+        config.setWindowedMode(GameSettings.SCREEN_WIDTH / 2, GameSettings.SCREEN_HEIGHT / 2);
         new Lwjgl3Application(new MyGdxGame(), config);
     }
 }
